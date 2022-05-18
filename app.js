cd7921f69d0a28ea34127d922105761ca7433413
Liidia Laada <liidia.laada@gmail.com>
2022-05-18 18:56:18 +0300

    Object, array, function, literal, union types, enums, classes, interfaces, generiic types, TS compiller.

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
-var num1Input = document.getElementById('num1');
-var num2Input = document.getElementById('num2');
-var buttonElement = document.querySelector('button');
+"use strict";
+const num1Input = document.getElementById('num1');
+const num2Input = document.getElementById('num2');
+const buttonElement = document.querySelector('button');
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    print() {
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor(name, age, permissions) {
+        super(name, age);
+        this.permissions = permissions;
+    }
+}
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+;
+const results = [];
 function add(a, b) {
     return a + b;
 }
-function printResult(result) {
-    console.log(result);
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
 }
-buttonElement.addEventListener('click', function () {
-    var num1 = +num1Input.value;
-    var num2 = +num2Input.value;
-    var result = add(num1, num2);
-    printResult(result);
+buttonElement.addEventListener('click', () => {
+    const num1 = +num1Input.value;
+    const num2 = +num2Input.value;
+    const result = add(num1, num2);
+    const resultContainer = {
+        res: result,
+        print() {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
 });
+function logAndEcho(val) {
+    console.log(val);
+    return val;
+}
+logAndEcho('Hi there').split(' ');
+tsc;
--- app.ts
+++ app.ts
@@ -1,13 +1,61 @@
 const num1Input = document.getElementById('num1')  as HTMLInputElement;
 const num2Input = <HTMLInputElement>document.getElementById('num2');
-const buttonElement = document.querySelector('button');
+const buttonElement = document.querySelector('button')!;
+
+// class User {
+//     name: string;
+//     private age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+interface Greetable {
+    name: string;
+}
+
+interface Printable {
+    print(): void;
+}
+
+class User implements Greetable, Printable {
+    constructor(public name: string, private age: number) {}
+
+    print() {
+        console.log(this.name);
+    }
+}
+
+class Admin extends User {
+    constructor(name: string, age: number, private permissions: string[]) {
+        super(name, age);
+    }
+}
+
+interface CalculationContainer {
+    res: number;
+    print(): void;
+}
+type CalculationResults = CalculationContainer[];
+type PrintMode = 'console' | 'alert';
+enum OutputMode { CONSOLE , ALERT };
+
+const results: CalculationResults = [];
 
 function add(a: number,b: number) {
     return a + b;
 }
 
-function printResult(result: any) : void {
-    console.log(result);
+function printResult(result: any, printMode: OutputMode) : void {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+
+    if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }    
 }
 
 buttonElement.addEventListener('click', () => {
@@ -15,5 +63,22 @@ buttonElement.addEventListener('click', () => {
     const num2 = +num2Input.value;
 
     const result = add(num1, num2);
-    printResult(result);
+    const resultContainer = {
+        res: result,
+        print() {
+            console.log(this.res);
+        }
+    };
+
+    results.push(resultContainer);
+
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
 });
+
+function logAndEcho<T>(val: T) {
+    console.log(val);
+    return val;
+}
+
+logAndEcho<string>('Hi there').split(' ');
